dd0bb52772ceaef4036ad50e1264ba8fc7e12d18
Calin Nicolai <kalinnikolay02@gmail.com>
2024-09-12 10:17:16 +0300

    Added ' symbole to regex

--- public/JS/Auth.js
+++ public/JS/Auth.js
@@ -1,24 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var form = document.forms['form'];
 
-    form.addEventListener('submit', function(e) {
-        // Сбрасываем ошибки
+    form.addEventListener('submit', function (e) {
         var nameErr = document.getElementById('name-err');
         var passwordErr = document.getElementById('password-err');
         nameErr.innerHTML = '';
         passwordErr.innerHTML = '';
 
-        // Получаем значения полей
         var name = document.getElementById('name').value;
         var password = document.getElementById('password').value;
 
-        // Регулярное выражение для проверки наличия специальных символов
-        var invalidChars = /[!@#$%^&*(),.?":{}|<>]/g;
+        var invalidChars = /['!@#$%^&*(),.?":{}|<>]/g;
 
-        // Флаг для отмены отправки формы
         var hasError = false;
 
-        // Проверка поля "name"
         if (name === '') {
             nameErr.innerHTML = 'Поле не может быть пустым';
             hasError = true;
@@ -27,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
             hasError = true;
         }
 
-        // Проверка поля "password"
         if (password === '') {
             passwordErr.innerHTML = 'Поле не может быть пустым';
             hasError = true;
@@ -36,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
             hasError = true;
         }
 
-        // Если есть ошибки, отменяем отправку формы
         if (hasError) {
             e.preventDefault();
         }
